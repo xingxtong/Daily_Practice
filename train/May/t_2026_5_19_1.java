d67d38ab2f42640c3f07ee70961338c33dc8e8c4
星瞳 <oncwnuGv3RULMTcsKdT_ULAhPraY@git.weixin.qq.com>
2026-05-21 08:35:50 +0800

    2026.5.19的题目

---
+++ train/May/t_2026_5_19_1.java
@@ -0,0 +1,62 @@
+package train.May;
+
+import java.util.Scanner;
+
+public class t_2026_5_19_1
+{
+    //第三题
+    public static void main(String[] args)
+    {
+        System.out.println("请输入1-10^9内的整数");
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        for(int i=1;i*i<=n;i++)
+        {
+            System.out.print((i*i)+" ");
+        }
+    }
+    //第三题
+    public static void train_3(int n)
+    {
+        for(int i=1;i*i<n;i++)
+        {
+            System.out.print((i*i)+" ");
+        }
+    }
+    public static void train_4(boolean[] arr,int num)
+    {
+        for(int i=1;i<=num;i++)
+        {
+            for(int j=i;j<=num;j+=i)
+            {
+                //切换状态
+                arr[j]=!arr[j];
+            }
+        }
+        for(int i=1;i<=num;i++)
+        {
+            if(arr[i])
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static void train_1()
+    {
+        Scanner inp = new Scanner(System.in);
+        int n=inp.nextInt();
+        int sum=0;
+        //计算年龄总和
+        for(int i=0;i<n;i++)
+        {
+            sum += inp.nextInt();
+        }
+        //直接输出平均数
+        double pj=(sum/(n*1.0));
+        System.out.printf("%.2f",pj);
+    }
+    public static void train_2()
+    {
+
+    }
+}
